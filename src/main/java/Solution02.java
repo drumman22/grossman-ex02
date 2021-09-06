a29e7b3a3e4b96a79f4989c324c16b81c0723bd2
drumman22 <grossmanjason0@gmail.com>
2021-09-05 22:11:52 -0400

    Initial commit

--- src/main/java/Solution02.java
+++ src/main/java/Solution02.java
@@ -1,3 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Jason Grossman
+ */
+
+/*
+-- Pseudocode --
+create input scanner
+ask for input string
+use scanner to get input
+print string and .length()
+ */
+
 import java.util.Scanner;
 
 public class Solution02 {
